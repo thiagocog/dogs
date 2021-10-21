23af94d00a9ebc839984780beaa764e3b4e3eef3
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-10-22 00:09:29 +0100

    header created

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
+import { ReactComponent as Dogs } from '../Assets/dogs.svg'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to='/' aria-label='Dogs - Home' className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to='/login' className={styles.login}>Login / Criar</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
