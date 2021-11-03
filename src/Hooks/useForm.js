c3db060cb69084562428f1a4ba9d863db196dde4
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-11-03 00:20:04 +0000

    photoPost ok

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,9 +5,13 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido'
   },
-  password : {
+  password: {
     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     message: 'A senha deve ter, no mínimo, 8 caracteres, contendo, pelo menos, um dígito, uma letra minúscula e uma letra maiúscula.'
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize apenas números'
   }
 }
 
--- src/Hooks/useMedia.js
+++ src/Hooks/useMedia.js
@@ -3,7 +3,7 @@ import React from 'react'
 const useMedia = (media) => {
 
   const [match, setMatch] = React.useState(null)
-
+  
   React.useEffect(() => {
     function changeMatch() {
       const { matches } = window.matchMedia(media)
