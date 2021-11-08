b2030570b8c740a34b27dc2ee8d24dfa9bb75549
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-11-08 19:29:55 +0000

    post comment ok

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,8 +1,25 @@
+import React from 'react'
+import styles from './PhotoComments.module.css'
+import { UserContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+
 const PhotoComments = ({ id, comments }) => {
+
+  const [commentsList, setCommentsList] = React.useState(() => comments)
+  const { login } = React.useContext(UserContext)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <ul className={styles.comments}>
+        {commentsList.map(comment => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={id} setCommentsList={setCommentsList} />}
+    </>
   )
 }
 
---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import useFetch from '../../Hooks/useFetch'
+import { COMMENT_POST } from '../../api'
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg'
+import Error from '../Helper/Error'
+
+const PhotoCommentsForm = ({ id, setCommentsList }) => {
+
+  const token = localStorage.getItem('token')
+  const [comment, setComment] = React.useState('')
+  const { error, request } = useFetch()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const { url, options } = COMMENT_POST(id, token, { comment })
+    const { response, resJson } = await request(url, options)
+    if (response.ok) {
+      setComment('')
+      setCommentsList(commentsList => [
+        ...commentsList,
+        resJson
+      ])
+    } 
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id='comment'
+        name='comment'
+        placeholder='Make your comment'
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button><Enviar /></button>
+      <Error error={error} />
+    </form>
+  )
+}
+
+export default PhotoCommentsForm
