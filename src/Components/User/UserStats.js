2570b6886f5ac7e4383a012fd252023f2fb86ca6
Thiago Carvalho de Onofre Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2021-11-25 15:23:24 -0300

    victory graphs ok

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import Head from '../Helper/Head'
+import useFetch from '../../Hooks/useFetch'
+import Loading from '../Helper/Loading'
+import Error from '../Helper/Error'
+import UserStatsGraphs from './UserStatsGraphs'
+import { STATS_GET } from '../../api'
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Stats' />
-      Estatísticas do Usuário
-    </div>
-  )
+
+  const { data, loading, error, request } = useFetch()
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = localStorage.getItem('token')
+      const { url, options } = STATS_GET(token)
+      await request(url, options)
+    }
+    getData()
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data) {
+    return (
+      <div>
+        <Head title='Stats' />
+        <UserStatsGraphs data={data} />
+      </div>
+    )
+  }
+  return null
 }
 
 export default UserStats
